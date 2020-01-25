9947da8cb0d32613b5eeb9466187e4d0a209e18e
epi052 <epibar052@gmail.com>
2020-01-25 15:05:47 -0600

    testing addition of tests

---
+++ tests/test_install_command.py
@@ -0,0 +1,33 @@
+import shutil
+import importlib
+import subprocess
+from pathlib import Path
+
+import utils
+from recon.config import tool_paths
+
+recon_pipeline = importlib.import_module("recon-pipeline")
+
+
+def test_install_masscan():
+    masscan = Path(tool_paths.get("masscan"))
+
+    utils.setup_install_test(masscan)
+
+    rs = recon_pipeline.ReconShell()
+
+    utils.run_cmd(rs, "install masscan")
+
+    assert masscan.exists() is True
+
+
+def test_install_amass():
+    utils.setup_install_test()
+
+    subprocess.run("apt remove amass -y".split())
+
+    rs = recon_pipeline.ReconShell()
+
+    utils.run_cmd(rs, "install amass")
+
+    assert shutil.which("amass") is not None
---
+++ tests/utils.py
@@ -0,0 +1,55 @@
+import sys
+from pathlib import Path
+from contextlib import redirect_stdout, redirect_stderr
+
+from cmd2.utils import StdSim
+
+
+def normalize(block):
+    """ Normalize a block of text to perform comparison.
+    Strip newlines from the very beginning and very end  Then split into separate lines and strip trailing whitespace
+    from each line.
+    """
+    assert isinstance(block, str)
+    block = block.strip("\n")
+    return [line.rstrip() for line in block.splitlines()]
+
+
+def run_cmd(app, cmd):
+    """ Clear out and err StdSim buffers, run the command, and return out and err """
+    saved_sysout = sys.stdout
+    sys.stdout = app.stdout
+
+    # This will be used to capture app.stdout and sys.stdout
+    copy_cmd_stdout = StdSim(app.stdout)
+
+    # This will be used to capture sys.stderr
+    copy_stderr = StdSim(sys.stderr)
+
+    try:
+        app.stdout = copy_cmd_stdout
+        with redirect_stdout(copy_cmd_stdout):
+            with redirect_stderr(copy_stderr):
+                app.onecmd_plus_hooks(cmd)
+    finally:
+        app.stdout = copy_cmd_stdout.inner_stream
+        sys.stdout = saved_sysout
+
+    out = copy_cmd_stdout.getvalue()
+    err = copy_stderr.getvalue()
+    return normalize(out), normalize(err)
+
+
+def setup_install_test(tool=None):
+    tools = Path.home() / ".cache" / ".tool-dict.pkl"
+
+    try:
+        tools.unlink()
+    except FileNotFoundError:
+        pass
+
+    if tool is not None:
+        try:
+            tool.unlink()
+        except FileNotFoundError:
+            pass
