b4cda246ee5026e2bc1cc350d0bebce710daeae1
epi <epibar052@gmail.com>
2020-03-28 17:39:28 -0500

    updated amass tests and pre-commit version

--- tests/test_recon/test_amass.py
+++ tests/test_recon/test_amass.py
@@ -1,8 +1,9 @@
+import tempfile
 from pathlib import Path
 
 import luigi
 
-from pipeline.recon import ParseAmassOutput
+from pipeline.recon import AmassScan, ParseAmassOutput
 
 tfp = "../data/bitdiscovery"
 tf = Path(tfp).stem
@@ -38,12 +39,58 @@ subdomains = [
 amass_json = Path(__file__).parent.parent / "data" / "recon-results" / "amass-results" / "amass.json"
 
 
-def test_parse_amass_ip_results(tmp_path):
-    pao = ParseAmassOutput(
-        target_file=tf, exempt_list=el, results_dir=str(tmp_path), db_location=str(Path(tmp_path) / "testing.sqlite")
-    )
+class TestAmassScan:
+    def setup_method(self):
+        self.tmp_path = Path(tempfile.mkdtemp())
+        self.asc = AmassScan(
+            target_file=tf,
+            exempt_list=el,
+            results_dir=str(self.tmp_path),
+            db_location=str(self.tmp_path / "testing.sqlite"),
+        )
 
-    pao.input = lambda: luigi.LocalTarget(amass_json)
-    pao.run()
+    def test_parse_amass_creates_database(self):
+        assert self.asc.db_mgr.location.exists()
+        assert self.tmp_path / "testing.sqlite" == self.asc.db_mgr.location
 
-    assert pao.output().exists()
+    def test_amassscan_output_location(self):
+        assert self.asc.output().path == str(Path(self.tmp_path) / "amass-results" / "amass.json")
+
+
+class TestParseAmass:
+    def setup_method(self):
+        self.tmp_path = Path(tempfile.mkdtemp())
+        self.pao = ParseAmassOutput(
+            target_file=tf,
+            exempt_list=el,
+            results_dir=str(self.tmp_path),
+            db_location=str(self.tmp_path / "testing.sqlite"),
+        )
+
+        self.pao.input = lambda: luigi.LocalTarget(amass_json)
+        self.pao.run()
+
+    def test_parse_amass_ip_results(self):
+        assert self.pao.output().exists()
+
+    def test_parse_amass_hostname_results(self):
+        parsed_hosts = self.pao.db_mgr.get_all_hostnames()
+        for hostname in subdomains:
+            assert hostname in parsed_hosts
+
+    def test_parse_amass_ipv4_results(self):
+        parsed_hosts = self.pao.db_mgr.get_all_ipv4_addresses()
+        for ip in ips:
+            assert ip in parsed_hosts
+
+    def test_parse_amass_ipv6_results(self):
+        parsed_hosts = self.pao.db_mgr.get_all_ipv6_addresses()
+        for ip in ip6s:
+            assert ip in parsed_hosts
+
+    def test_parse_amass_creates_results_dir(self):
+        assert self.pao.results_subfolder.exists()
+
+    def test_parse_amass_creates_database(self):
+        assert self.pao.db_mgr.location.exists()
+        assert self.tmp_path / "testing.sqlite" == self.pao.db_mgr.location
