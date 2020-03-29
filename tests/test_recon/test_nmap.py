43d2c6d9c7869083e06b353e0cbbef9e68fc2d85
epi <epibar052@gmail.com>
2020-03-28 20:39:58 -0500

    updated nmap tests

--- tests/test_recon/test_nmap.py
+++ tests/test_recon/test_nmap.py
@@ -1,45 +1,49 @@
+import tempfile
 from pathlib import Path
-
-from pipeline.recon import ThreadedNmapScan, SearchsploitScan
 
 import luigi
 
 from ..utils import is_kali
-
-tfp = "../data/bitdiscovery"
-tf = Path(tfp).stem
-el = "../data/blacklist"
-rd = "../data/recon-results"
+from pipeline.recon import ThreadedNmapScan, SearchsploitScan
 
 nmap_results = Path(__file__).parent.parent / "data" / "recon-results" / "nmap-results"
 
 
-def test_nmap_output_location(tmp_path):
-    tns = ThreadedNmapScan(
-        target_file=tf,
-        exempt_list=el,
-        results_dir=str(tmp_path),
-        top_ports=100,
-        db_location=str(Path(tmp_path) / "testing.sqlite"),
-    )
+class TestThreadedNmapScan:
+    def setup_method(self):
+        self.tmp_path = Path(tempfile.mkdtemp())
+        self.scan = ThreadedNmapScan(
+            target_file=__file__, results_dir=str(self.tmp_path), db_location=str(self.tmp_path / "testing.sqlite")
+        )
 
-    assert tns.output().get("localtarget").path == str(Path(tmp_path) / "nmap-results")
+    def test_scan_creates_results_dir(self):
+        assert self.scan.results_subfolder == self.tmp_path / "nmap-results"
+
+    def test_scan_creates_database(self):
+        assert self.scan.db_mgr.location.exists()
+        assert self.tmp_path / "testing.sqlite" == self.scan.db_mgr.location
+
+    def test_scan_output_location(self):
+        assert self.scan.output().get("localtarget").path == str(self.tmp_path / "nmap-results")
 
 
-def test_searchsploit_produces_results(tmp_path):
-    sss = SearchsploitScan(
-        target_file=tf,
-        exempt_list=el,
-        results_dir=str(tmp_path),
-        top_ports=100,
-        db_location=str(Path(tmp_path) / "testing.sqlite"),
-    )
+class TestSearchsploitScan:
+    def setup_method(self):
+        self.tmp_path = Path(tempfile.mkdtemp())
+        self.scan = SearchsploitScan(
+            target_file=__file__, results_dir=str(self.tmp_path), db_location=str(self.tmp_path / "testing.sqlite")
+        )
 
-    sss.input = lambda: luigi.LocalTarget(nmap_results)
+    def test_scan_creates_database(self):
+        assert self.scan.db_mgr.location.exists()
+        assert self.tmp_path / "testing.sqlite" == self.scan.db_mgr.location
 
-    if not is_kali():
-        return True
+    def test_searchsploit_produces_results(self):
+        self.scan.input = lambda: {"localtarget": luigi.LocalTarget(nmap_results)}
 
-    sss.run()
+        if not is_kali():
+            return True
 
-    assert len([x for x in Path(sss.output().path).glob("searchsploit*.txt")]) > 0
+        self.scan.run()
+
+        assert len([x for x in (nmap_results / ".." / "searchsploit-results").glob("searchsploit*.txt")]) > 0
