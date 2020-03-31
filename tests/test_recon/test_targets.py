63b6c47ca7dc7a8ed3cc51bc949b291121fe9c88
epi <epibar052@gmail.com>
2020-03-31 06:43:00 -0500

    updated targets tests

--- tests/test_recon/test_targets.py
+++ tests/test_recon/test_targets.py
@@ -1,3 +1,4 @@
+import shutil
 import tempfile
 from pathlib import Path
 
@@ -9,9 +10,18 @@ tfp = Path(__file__).parent.parent / "data" / "bitdiscovery"
 class TestReconTargets:
     def setup_method(self):
         self.tmp_path = Path(tempfile.mkdtemp())
+
+        shutil.copy(tfp, self.tmp_path)
+        Path(self.tmp_path / "bitdiscovery").open(mode="a").writelines(["127.0.0.1"])
+
         self.scan = TargetList(
-            target_file=str(tfp), results_dir=str(self.tmp_path), db_location=str(self.tmp_path / "testing.sqlite")
+            target_file=str(self.tmp_path / "bitdiscovery"),
+            results_dir=str(self.tmp_path),
+            db_location=str(self.tmp_path / "testing.sqlite"),
         )
+
+    def teardown_method(self):
+        shutil.rmtree(self.tmp_path)
 
     def test_scan_creates_database(self):
         assert self.scan.db_mgr.location.exists()
