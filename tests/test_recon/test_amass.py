7299e2d3597dab5a66421d860b33e6e7c3560c97
epi <epibar052@gmail.com>
2020-03-31 06:33:17 -0500

    updated amass tests

--- tests/test_recon/test_amass.py
+++ tests/test_recon/test_amass.py
@@ -1,9 +1,11 @@
+import shutil
 import tempfile
 from pathlib import Path
+from unittest.mock import patch, MagicMock
 
 import luigi
 
-from pipeline.recon import AmassScan, ParseAmassOutput
+from pipeline.recon import AmassScan, ParseAmassOutput, TargetList
 
 
 amass_json = Path(__file__).parent.parent / "data" / "recon-results" / "amass-results" / "amass.json"
@@ -15,6 +17,27 @@ class TestAmassScan:
         self.scan = AmassScan(
             target_file=__file__, results_dir=str(self.tmp_path), db_location=str(self.tmp_path / "testing.sqlite")
         )
+
+    def teardown_method(self):
+        shutil.rmtree(self.tmp_path)
+
+    def test_scan_requires(self):
+        with patch("pipeline.recon.TargetList"):
+            retval = self.scan.requires()
+            assert isinstance(retval, TargetList)
+
+    def test_scan_run(self):
+        with patch("subprocess.run") as mocked_run:
+            self.scan.run()
+            assert mocked_run.called
+
+    def test_scan_run_with_hostnames(self):
+        with patch("subprocess.run") as mocked_run:
+            self.scan.db_mgr = MagicMock()
+            self.scan.db_mgr.get_all_hostnames.return_value = ["google.com"]
+            self.scan.exempt_list = "stuff"
+            self.scan.run()
+            assert mocked_run.called
 
     def test_scan_creates_database(self):
         assert self.scan.db_mgr.location.exists()
@@ -32,6 +55,14 @@ class TestParseAmass:
         )
         self.scan.input = lambda: luigi.LocalTarget(amass_json)
         self.scan.run()
+
+    def teardown_method(self):
+        shutil.rmtree(self.tmp_path)
+
+    def test_scan_requires(self):
+        with patch("pipeline.recon.AmassScan"):
+            retval = self.scan.requires()
+            assert isinstance(retval, AmassScan)
 
     def test_scan_results(self):
         assert self.scan.output().exists()
