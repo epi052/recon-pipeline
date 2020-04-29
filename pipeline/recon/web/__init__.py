796e4a6d0b7e626e011b63075a7c2aef710a26f3
epi <epibar052@gmail.com>
2020-04-29 11:13:55 -0500

    added basic PoC for waybackurls scanner; updated helpers.py test

--- pipeline/recon/web/__init__.py
+++ pipeline/recon/web/__init__.py
@@ -2,4 +2,5 @@ from .aquatone import AquatoneScan
 from .gobuster import GobusterScan
 from .targets import GatherWebTargets
 from .webanalyze import WebanalyzeScan
+from .waybackurls import WaybackurlsScan
 from .subdomain_takeover import SubjackScan, TKOSubsScan
